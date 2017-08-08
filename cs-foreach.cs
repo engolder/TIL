9656c87eebda57c5bd8cd2e880423db188b1a317
yunedoopea2 <yunedoopea610@gmail.com>
2017-08-08 21:24:43 +0900

    C#'s loop statement, arrangement

---
+++ cs-foreach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            //C#의 배열선언
+
+            foreach(int elem in arr)
+            {
+                Console.WriteLine(elem);
+            }
+            //C#의 foreach문
+        }
+    }
+}

ccfd45ceadc9c1840688bd28731660276509ea8a
yunedoopea2 <yunedoopea610@gmail.com>
2017-08-12 18:03:33 +0900

    cs-cctor

---
+++ cs-cctor.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: cs, Configuration: Debug Any CPU ------
+1>  cs -> C:\Users\dsm2017\Documents\Visual Studio 2017\Projects\cs\cs\bin\Debug\cs.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========

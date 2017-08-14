754963fe068da449e4e62d1e0d1c0bd49ebe25de
yunedoopea2 <yunedoopea610@gmail.com>
2017-08-14 22:49:06 +0900

    cs-inheritance3

---
+++ cs-inheritance3.cs
@@ -0,0 +1,4 @@
+﻿1>------ Build started: Project: cs, Configuration: Debug Any CPU ------
+1>D:\github\study\cs-inheritance2.cs(18,14,18,24): warning CS0649: Field 'Notebook.fingerScan' is never assigned to, and will always have its default value false
+1>  cs -> C:\Users\dsm2017\Documents\Visual Studio 2017\Projects\cs\cs\bin\Debug\cs.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========

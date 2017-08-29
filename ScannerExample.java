970cfb2a495d04afde9c08cc3ae01e233113cabe
yunedoopea2 <yunedoopea610@gmail.com>
2017-08-29 22:23:21 +0900

    java study

---
+++ ScannerExample.java
@@ -0,0 +1,10 @@
+package study;
+import java.util.Scanner;
+
+public class ScannerExample {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		int age = scanner.nextInt();
+		System.out.println(age);
+	}
+}

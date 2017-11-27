03a9ba49bab49556f0805dd154149a77bd821e88
yunedoopea610@gmail.com <yunedoopea610@gmail.com>
2017-11-27 22:57:04 +0900

    Update RealTimeSerchWords.java
    add daum's real-time rank

--- Projects-to-study/RTSW/src/source/RealTimeSerchWords.java
+++ Projects-to-study/RTSW/src/source/RealTimeSerchWords.java
@@ -6,14 +6,38 @@ import org.jsoup.select.Elements;
 public class RealTimeSerchWords {
 
 	public static void main(String[] args) throws Exception {
-		Document document = Jsoup.connect("https://www.naver.com/").get();
-        
+		
+		Document document;
+		
+		// naver
+		document = Jsoup.connect("https://www.naver.com/").get();
         if (document != null) {
-            Elements elements = document.select("ul > li.ah_item[data-order]");
+        	
+        	// [data-order]를 포함하는 li와 포함하지 않는 li두 개 존재
+        	// 선택자에 data-order를 포함하지 않으면 같은 내용이 두 번 반복돼서 출력됨
+            Elements elements = document.select("ul > li.ah_item[data-order] a.ah_a > span.ah_k");
+            
+            System.out.println("\n네이버");
+            System.out.println("===========================================");
+            
             for (int i = 0; i < elements.size(); i++) {
-                System.out.println("검색어 : " + elements.get(i).select("a.ah_a > span.ah_k").html());
-                System.out.println("랭킹 : " + elements.get(i).select("a.ah_a > span.ah_r").html());
-                System.out.println("------------------------------------------");
+                System.out.print((i+1) + ". ");
+                System.out.println("검색어 : " + elements.get(i).html());
+            }
+        }
+        
+        // daum
+        document = Jsoup.connect("https://www.daum.net/").get();
+        if (document != null) {
+        	
+            Elements elements = document.select("ol.issue_row > li a.link_issue[tabindex]");
+            
+            System.out.println("\n다음");
+            System.out.println("===========================================");
+            
+            for (int i = 0; i < elements.size(); i++) {
+                System.out.print((i+1) + ". ");
+                System.out.println("검색어 : " + elements.get(i).html());
             }
         }
 	}

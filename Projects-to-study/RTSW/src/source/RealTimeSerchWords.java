9dbe344ce343d242347c8a0bcfbaf563eaa14bf4
yunedoopea610@gmail.com <yunedoopea610@gmail.com>
2017-11-27 23:46:22 +0900

    Add zum's real-time rank

--- Projects-to-study/RTSW/src/source/RealTimeSerchWords.java
+++ Projects-to-study/RTSW/src/source/RealTimeSerchWords.java
@@ -13,16 +13,17 @@ public class RealTimeSerchWords {
 		document = Jsoup.connect("https://www.naver.com/").get();
         if (document != null) {
         	
-        	// [data-order]를 포함하는 li와 포함하지 않는 li두 개 존재
-        	// 선택자에 data-order를 포함하지 않으면 같은 내용이 두 번 반복돼서 출력됨
+        	// [data-order]로 중복 방지
+        		/*  [data-order]를 포함하는 li와 포함하지 않는 li두 개 존재
+        			선택자에 data-order를 포함하지 않으면 같은 내용이 두 번 반복돼서 출력됨 */
             Elements elements = document.select("ul > li.ah_item[data-order] a.ah_a > span.ah_k");
             
             System.out.println("\n네이버");
             System.out.println("===========================================");
             
             for (int i = 0; i < elements.size(); i++) {
-                System.out.print((i+1) + ". ");
-                System.out.println("검색어 : " + elements.get(i).html());
+                System.out.print((i+1) + ". "); // rank
+                System.out.println(elements.get(i).html()); // word
             }
         }
         
@@ -30,14 +31,49 @@ public class RealTimeSerchWords {
         document = Jsoup.connect("https://www.daum.net/").get();
         if (document != null) {
         	
+        	// [tabindex]로 중복 방지
             Elements elements = document.select("ol.issue_row > li a.link_issue[tabindex]");
             
             System.out.println("\n다음");
             System.out.println("===========================================");
             
             for (int i = 0; i < elements.size(); i++) {
-                System.out.print((i+1) + ". ");
-                System.out.println("검색어 : " + elements.get(i).html());
+                System.out.print((i+1) + ". "); // rank
+                System.out.println(elements.get(i).html()); // word
+            }
+        }
+        
+        // nate
+        /*
+        document = Jsoup.connect("http://www.nate.com/?f=autorefresh").get();
+        if (document != null) {
+        	
+            Elements elements = document.select(".kwd_list li");
+            
+            System.out.println("\n네이트");
+            System.out.println("===========================================");
+            
+            System.out.println(elements.size());
+            for (int i = 0; i < elements.size(); i++) {
+                System.out.print((i+1) + ". "); // rank
+                System.out.println(elements.get(i).html()); // word
+            }
+        }
+        */
+        
+        // zum
+        document = Jsoup.connect("http://zum.com/#!/home").get();
+        if (document != null) {
+        	
+        	// [tabindex]로 중복 방지
+            Elements elements = document.select("ul.rank_list a.d_ready span");
+            
+            System.out.println("\n줌");
+            System.out.println("===========================================");
+            
+            for (int i = 0; i < elements.size(); i++) {
+                System.out.print((i+1) + ". "); // rank
+                System.out.println(elements.get(i).html()); // word
             }
         }
 	}

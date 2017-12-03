cb54a5b3a614e057101d4ef43ad694a5ff61f4dc
Jun-Mo <yunedoopea610@gmail.com>
2017-12-03 22:45:16 +0900

    Fix many bugs & change manything

--- Projects-to-study/RTSW(web)/index.js
+++ Projects-to-study/RTSW(web)/index.js
@@ -9,61 +9,90 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-
-/* parsing */
-
 let urlNaver = "https://www.naver.com/";
 let jsonNaver = [];
 let urlDaum = "https://www.daum.net/";
 let jsonDaum = [];
 let urlZum = "http://zum.com/";
 let jsonZum = [];
+let urlNate = "http://www.nate.com/nate/LiveKeyword";
+let jsonNate = [];
 
-// naver
-request(urlNaver, function(err, response, body) {  
-    if(err) {
-        console.log();
-    } else {
-        let $ = cheerio.load(body);
-        let data = $("ul.ah_l[data-list] li.ah_item");
-        data.each(function() {
-            let searchWord = $(this).find("span.ah_k").text();
-            jsonNaver.push({searchWord : searchWord});
-        });
-    }
 
-    // daum
-    request(urlDaum, function(err, response, body) {  
+/* parsing */
+function loadRTSW(){
+    // naver
+    request(urlNaver, function(err, response, body) {  
         if(err) {
             console.log();
         } else {
             let $ = cheerio.load(body);
-            let data = $("div.realtime_part > ol.list_hotissue.issue_row li");
+            let data = $("ul.ah_l[data-list] li.ah_item");
+            jsonNaver = [];
             data.each(function() {
-                let searchWord = $(this).find("a.link_issue[tabindex]").text();
-                jsonDaum.push({searchWord : searchWord});
+                let searchWord = $(this).find("span.ah_k").text();
+                jsonNaver.push({searchWord : searchWord});
             });
         }
 
-        // zum
-        request(urlZum, function(err, response, body) {  
+        // daum
+        request(urlDaum, function(err, response, body) {  
             if(err) {
                 console.log();
             } else {
                 let $ = cheerio.load(body);
-                let data = $("li.d_rank");
+                let data = $("div.realtime_part > ol.list_hotissue.issue_row li");
+                jsonDaum = [];
                 data.each(function() {
-                    let searchWord = $(this).find("a.d_btn_keyword.d_ready").text();
-                    jsonZum.push({searchWord : searchWord});
+                    let searchWord = $(this).find("a.link_issue[tabindex]").text();
+                    jsonDaum.push({searchWord : searchWord});
                 });
             }
-            
-            /* router */
-            app.get('/',function(req,res){
-                res.render('index', {jsonNaver : jsonNaver,jsonDaum : jsonDaum, jsonZum : jsonZum});
+
+            // zum
+            request(urlZum, function(err, response, body) {  
+                if(err) {
+                    console.log();
+                } else {
+                    let $ = cheerio.load(body);
+                    let data = $("li.d_rank");
+                    jsonZum = [];
+                    data.each(function() {
+                        let searchWord = $(this).find("a.d_btn_keyword.d_ready").text();
+                        jsonZum.push({searchWord : searchWord});
+                    });
+                }
+                
+                // nate
+                // request({uri: urlNate, encoding: 'euc-kr'}, function(err, response, body) {  
+                    // if(err) {
+                    //     console.log();
+                    // } else {
+                    //     let searchWord;
+                    //     console.log(body);
+                    //     eval(body.replace("RSKS.Init();", ""));
+                    //     array = JSON.parse(arrHotRecent);
+                    //     array.forEach(key => {
+                    //        jsonNate.push({searchWord : key[1]});
+                    //     });
+                    // }
+
+                // });
             });
         });
     });
+}
+
+var timeInMs = Date.now();
+loadRTSW();
+
+/* router */
+app.get('/',function(req, res){
+    if((Date.now() - timeInMs) > 3000){
+        loadRTSW();
+        timeInMs = Date.now()
+    }
+    res.render('index', {jsonNaver : jsonNaver,jsonDaum : jsonDaum, jsonZum : jsonZum});
 });
 
 /* Open server */
